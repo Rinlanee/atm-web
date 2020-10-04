39f7f9a94cfbd52594065ef10d2ea18e0a1bc758
Rinlanee <punch.rinlanee@gmail.com>
2020-10-04 23:39:30 +0700

    add feature edit

--- src/main/java/th/ac/ku/atm/controller/BankAccountController.java
+++ src/main/java/th/ac/ku/atm/controller/BankAccountController.java
@@ -2,10 +2,7 @@ package th.ac.ku.atm.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import th.ac.ku.atm.classes.BankAccount;
 import th.ac.ku.atm.service.BankAccountService;
 
@@ -31,4 +28,22 @@ public class BankAccountController {
         model.addAttribute("bankaccounts", bankAccountService.getBankAccounts());
         return "redirect:bankaccount"; // redirect: used to prevent calling this method again; after ':' <URI>
     }
+
+    @GetMapping("/edit/{id}")
+    public String getEditBankAccountPage(@PathVariable int id, Model model) {
+        BankAccount bankAccount = bankAccountService.getBankAccount(id);
+        model.addAttribute("bankAccount", bankAccount);
+        return "bankaccount-edit";
+    }
+
+    @PostMapping("/edit/{id}")
+    public String editAccount(@PathVariable int id,
+                              @ModelAttribute BankAccount bankAccount,
+                              Model model) {
+
+        bankAccountService.editBankAccount(bankAccount);
+        model.addAttribute("bankaccounts",bankAccountService.getBankAccounts());
+        return "redirect:/bankaccount";
+    }
+
 }
